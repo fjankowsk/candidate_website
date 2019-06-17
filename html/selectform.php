4cc2b82c3249d4f340748a274ea4bf41999fe694
Fabian Jankowski <fjankowsk@gmail.com>
2019-06-17 21:12:53 +0100

    Provide beam start and end text input fields. Tweak their sizes.

--- html/selectform.php
+++ html/selectform.php
@@ -20,10 +20,13 @@ $_SESSION['pointing'] = $pointing;
 
 // select form
 echo "<form>\n";
-echo "<label for='pointing'>Pointing</label>\n";
-echo "<input type='text' name='pointing' id='pointing' value='" . $pointing . "' />\n";
-echo "<label for='beam'>Beam</label>\n";
-echo "<input type='text' name='beam' id='beam' />\n";
+echo "<label for='pointing'>Pointing:</label>\n";
+echo "<input type='text' name='pointing' id='pointing' size='6' maxlength='6' value='" . $pointing . "' />\n";
+
+echo "<label for='beam_start'>Beams:</label>\n";
+echo "<input type='text' name='beam_start' id='beam_start' size='4' maxlength='4' />\n";
+echo "to";
+echo "<input type='text' name='beam_end' id='beam_end' size='4' maxlength='4' />\n";
 echo "<input type='submit' value='Submit' />\n";
 echo "</form>\n";
 
