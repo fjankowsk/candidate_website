8695b0dc62c1db6fce686cb750e45ab2018d778d
Fabian Jankowski <fjankowsk@gmail.com>
2019-06-18 14:17:08 +0100

    Add CSS style sheet and rename grid view.

--- html/header.php
+++ html/header.php
@@ -4,13 +4,14 @@
   <title>MeerTRAP Candidate Viewer</title>
   <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
   <meta name='author' content='Fabian Jankowski' />
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div align='center'>
   <h1>MeerTRAP Candidate Viewer</h1>
   <p>
     [
-    <a href='index.php'>Candidate View</a> |
+    <a href='index.php'>Grid View</a> |
     <a href='overview.php'>Overview</a> |
     <a href='detailview.php'>Detail View</a>
     ]
